d4967990c59f3e7ec51b9a4cf6e3927a6f9e73b6
tong <reangsak1234@hotmail.com>
2024-10-03 14:12:41 +0700

    feat: tong-js-functions-assignment

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,24 @@
 // Start coding here
+const add = function (a, b) {
+  const result1 = a + b;
+  return result1;
+};
+console.log(add(10, 5));
+
+const subtract = function (a, b) {
+  const result2 = a - b;
+  return result2;
+};
+console.log(subtract(10, 5));
+
+const multiply = function (a, b) {
+  const result3 = a * b;
+  return result3;
+};
+console.log(multiply(10, 5));
+
+const divide = function (a, b) {
+  const result4 = a / b;
+  return result4;
+};
+console.log(divide(10, 5));
--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,24 @@
 // Start coding here
+const add = (a, b) => {
+  const result1 = a + b;
+  return result1;
+};
+console.log(add(10, 5));
+
+const subtract = (a, b) => {
+  const result2 = a - b;
+  return result2;
+};
+console.log(subtract(10, 5));
+
+const multiply = (a, b) => {
+  const result3 = a * b;
+  return result3;
+};
+console.log(multiply(10, 5));
+
+const divide = (a, b) => {
+  const result4 = a / b;
+  return result4;
+};
+console.log(divide(10, 5));
